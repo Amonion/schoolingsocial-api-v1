3ebc32122cb98371cb4798e54245fd9190fecd4d
Amonion <amonionprime@gmail.com>
2024-12-30 12:25:51 +0100

    first commit

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 const MONGO_URI = process.env.MONGO_URI_CLOUD || "";
 
+console.log(MONGO_URI);
+
 connectDB(MONGO_URI).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
