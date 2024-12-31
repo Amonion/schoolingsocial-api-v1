fd56a2d95a1e41bb4f95b805d752e2a84fb227d8
Amonion <amonionprime@gmail.com>
2024-12-31 19:10:02 +0100

    first commit

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,7 @@ app.use(
   })
 );
 app.use(bodyParser.json());
-app.use("/api/v1/places", placeRoutes);
+// app.use("/api/v1/places", placeRoutes);
 app.use("/api/v1/users", userRoutes);
 
 app.use((req, res, next) => {
